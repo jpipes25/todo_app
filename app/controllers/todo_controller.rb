8b0c8ce6405026fc5dabf811c14b9657d5fad592
Jonathan A Piper <ljpiper25@gmail.com>
2017-11-18 23:00:07 +0000

    Adding links to our list

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,38 @@
 class TodoController < ApplicationController
-   def index  
-   end
-   def show 
-      @todo_description = "Defrost the Dragon"
-@todo_pomodoro_estimate = 4
+   
+def show 
+todo_index = params[:id]
+
+if todo_index=="1"
+   @todo_description = "Feed Knuckles"
+   @todo_time_estimate= 3
+elsif todo_index== "2"
+   @todo_description = "Let chicken thaw"
+   @todo_time_estimate = 5
+elsif todo_index == '3'
+   @todo_description = "unload laundry"
+   @todo_time_estimate= 10
+elsif todo_index== "4"
+
+elsif todo_index=="5"
+   @todo_description = "empty dishwasher"
+   @todo_time_estimate= 20 
+elsif todo_index== "6"
+
+ elsif todo_index=="7"
+   @todo_description = "make history conclusion"
+   @todo_time_estimate= 6
+   elsif todo_index== "5"
+   @todo_description = "call grandma"
+   @todo_time_estimate= 5 
+elsif todo_index=="20" 
+@todo_description= "eat peppermint dumplings"
+@todo_time_estimate= 20
+
+   
       
-   end
+end
+end
+def index
+end
 end
