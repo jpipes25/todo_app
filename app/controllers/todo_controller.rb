b4c862181a6df4a8759c1f50b6ecf90a51171182
Jonathan A Piper <ljpiper25@gmail.com>
2017-11-19 22:14:04 +0000

    Adding wired update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,6 +6,25 @@ class TodoController < ApplicationController
   
   
  end
- def show
+ def new
+ end
+ def create
+    t = Todo.new
+    t.description = params['description']
+    t.pomodoro_estimate = params['pomodoro_estimate']
+    t.save
+    redirect_to "/todo/show/#{ t.id }"
+ end     
+ def edit
+    @todo = Todo.find_by_id(params[:id])
+ end
+ def update
+    t = Todo.find_by_id(params['id'])
+    t.description=params['description'] 
+    t.pomodoro_estimate=params['pomodoro_estimate']
+    t.save
+     redirect_to "/todo/show/#{ t.id }"
+    
  end
 end
+ 
